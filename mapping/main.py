f78003bf397594812076bcede659830739d824bf
Luke Shiner <luke@lukeshiner.com>
2018-02-15 17:11:28 +0000

    Change output to .xlsx.

--- mapping/main.py
+++ mapping/main.py
@@ -1,23 +1,26 @@
-import os
-
-from jinja2 import Template
+from tabler import Table
 
 from .channels import CHANNELS as channels
 from .productrange import PRODUCT_RANGES as ranges
-from .products import PRODUCTS as products
 
 
 class Main:
 
     def __init__(self, output_path):
-        template_path = os.path.join(
-            os.path.dirname(__file__), 'template.html')
 
-        with open(template_path, 'r') as template_file:
-            template = Template(template_file.read())
-        context = {
-            'products': products, 'channels': channels, 'ranges': ranges}
-        output = template.render(context)
+        header = ['Range SKU', 'Range Title', 'Stock Level', 'Search SKU']
+        for channel in channels:
+            header.append(channel.name)
+        data = []
 
-        with open(output_path, 'w', encoding='utf-8') as f:
-            f.write(output)
+        for product_range in ranges:
+            row = [
+                product_range.sku, product_range.title,
+                product_range.stock_level, product_range.products[0].sku]
+            for channel in channels:
+                if channel in product_range.channel_links:
+                    row.append(len(product_range.channel_links[channel]))
+                else:
+                    row.append(0)
+            data.append(row)
+        Table(header=header, data=data).write(output_path)
